87b9f43d888a4ebd5db3d621b8f2c74052ecd767
David Mlynski <tonystark2008em@gmail.com>
2021-11-28 16:28:58 -0800

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -3,15 +3,96 @@ let userAnswers = [];
 
 const questions = [
    {
-      question: "Whats 1+1?",
-      answers: ["-1", "your mom", "2"],
+      question: "The words “Life, Liberty, and the pursuit of Happiness” are in what founding document?",
+      answers: ["Declaration of Independence", "Bill of Rights", "The constitution", "Federalist papers"],
+      correct: 0
+   },
+   {
+      question: "How many U.S. senators are there?",
+      answers: ["100", "150", "50", "169"],
+      correct: 0
+   },
+   {
+      question: "How long is a term for a U.S. senator?",
+      answers: ["4", "8", "6", "2"],
       correct: 2
    },
    {
-      question: "What is an orange fruit?",
-      answers: ["apple", "orange", "not this one", "blueberry"],
+      question: "How many senators does each state have?",
+      answers: ["2", "5", "Varies state to state", "10"],
+      correct: 0
+   },
+   {
+      question: "How long is a term for a member of the House of Representatives?",
+      answers: ["4", "8", "6", "2"],
+      correct: 3
+   },
+   {
+      question: "Who appoints federal judges?",
+      answers: ["Supreme court", "Cabinet members", "The President", "Secretary of State"],
+      correct: 2
+   },
+   {
+      question: "How long is a term for a member of the House of Representatives?",
+      answers: ["4", "8", "6", "2"],
+      correct: 2
+   },
+   {
+      question: "Who signs bills to become laws?",
+      answers: ["Congress", "The President", "House of Representatives", "The Supreme Court"],
       correct: 1
-   }
+   },
+   {
+      question: "How many seats are on the Supreme Court?",
+      answers: ["5", "15", "9", "7"],
+      correct: 2
+   },
+   {
+      question: "How many Supreme Court justices are usually needed to decide a case?",
+      answers: ["5", "9", "11", "15"],
+      correct: 0
+   },
+   {
+      question: "When was the Declaration of Independence adopted?",
+      answers: ["July 4, 1776", "November 15, 1777", "July 4, 1775", "June 21, 1788"],
+      correct: 0
+   },
+   {
+      question: "Who was president during the Great Depression and World War II?",
+      answers: ["Richard Nixon", "Theodore Roosevelt", "Harry Truman", "Franklin Roosevelt"],
+      correct: 3
+   },
+   {
+      question: "What is the highest court in the United States?",
+      answers: ["Courts of Appeals", "Supreme Court", "Federal District Courts", "Court of Federal Claims"],
+      correct: 1
+   },
+   {
+      question: "Who is Commander in Chief of the U.S. military?",
+      answers: ["The President", "Congress", "The Senate", "The Vice President"],
+      correct: 0
+   },
+   {
+      question: "Which amendment states the President of the United States can serve only two terms?",
+      answers: ["42", "51", "89", "22"],
+      correct: 3
+   },
+   {
+      question: "Who does a U.S. senator represent?",
+      answers: ["Citizens from a congressional district", "Citizens from a state", "Citizens of the US", "Citizens from a city"],
+      correct: 1
+   },
+   {
+      question: "Who elects members of the House of Representatives?",
+      answers: ["Citizens from a congressional district", "Citizens from a state", "Citizens of the US", "Citizens from a city"],
+      correct: 0
+   },
+   {
+      question: "The President of the United States is in charge of which branch of government?",
+      answers: ["The Executive Branch", "The Judicial Branch", "The Legislative Branch", "The Representative Branch"],
+      correct: 0
+   },
+   
 ]
 
 function finishQuiz() {
